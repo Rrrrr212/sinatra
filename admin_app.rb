4aacb7e350f4ead210bdf22360d414b2aadf7f49
Rrrr <2823785414@qq.com>
2026-06-09 08:07:58 +0000

    9.7.2

--- admin_app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra/base'
-
-class AdminApp < Sinatra::Base
-  get '/' do
-    'AdminApp home'
-  end
-end
--- api_app.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra/base'
-require 'json'
-
-class ApiApp < Sinatra::Base
-  get '/' do
-    content_type :json
-    JSON.generate(app: 'ApiApp', status: 'ok')
-  end
-end
