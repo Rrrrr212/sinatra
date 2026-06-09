c0e4354e8de8f483bb4ae89de1a4d826ce355fdb
Rrrr <2823785414@qq.com>
2026-06-09 08:19:02 +0000

    9.7.3

--- admin_app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra/base'
-
-class AdminApp < Sinatra::Base
-  get '/' do
-    'Admin App'
-  end
-end
--- api_app.rb
+++
@@ -1,7 +0,0 @@
-require 'sinatra/base'
-
-class ApiApp < Sinatra::Base
-  get '/' do
-    'Api App'
-  end
-end
