de10165a4d24cd890d7f5cfe61c7b6a692fdc1f4
Rrrr <2823785414@qq.com>
2026-06-09 07:23:51 +0000

    9.5.1

--- test/integration/app.rb
+++ test/integration/app.rb
@@ -14,6 +14,21 @@ get '/ping' do
   'pong'
 end
 
+get '/logs' do
+  "logs"
+end
+
+get '/logs/stream' do
+  content_type 'text/event-stream'
+  stream(:keep_open) do |out|
+    until out.closed?
+      time = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+      out << "data: {\"time\":\"#{time}\"}\n\n"
+      sleep 1
+    end
+  end
+end
+
 get '/stream' do
   stream do |out|
     sleep 0.1
